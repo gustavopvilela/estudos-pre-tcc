1dacc6c568f3d247330b0f2a1f5b5ac88c9ca78c
gustavopvilela <gustavofga2016@gmail.com>
2022-01-11 19:55:31 -0300

    aula 02 - a função principal main()

---
+++ dart/logica-de-programacao-com-dart/aula02/funcao-principal.dart
@@ -0,0 +1,12 @@
+main(){
+  // tudo que será executado deverá ser colocado em main()
+  
+  print("Primeira impressão.");
+
+  print("Segunda impressão.");
+
+  print("Terceira impressão.");
+
+  print(25.6);
+  // dados numéricos não precisam de aspas, mas textuais, sim.
+}
