9a7d092b2e02c8511ae212cf47c15cbb452723e2
gustavopvilela <gustavofga2016@gmail.com>
2022-01-11 20:08:14 -0300

    aula 03 - variaveis

---
+++ dart/logica-de-programacao-com-dart/aula03 - variaveis/variaveis.dart
@@ -0,0 +1,15 @@
+main(){
+  // utiliza-se "var" para declarar variáveis
+  var imprimir = "Alguma coisa sendo impressa.";
+
+  print(imprimir);
+
+  imprimir = "Alguma outra coisa sendo impressa.";
+
+  print(imprimir);
+  // os valores das variáveis podem ser mudados
+
+  var numero = 98;
+
+  print(numero);
+}
