222226a004355d4604b09a7c0460cb627be891c2
gustavopvilela <gustavofga2016@gmail.com>
2022-01-12 14:43:37 -0300

    aulas 06, 07 e 08

--- dart/logica-de-programacao-com-dart/aula06 - estrutura de decisao/estrutura-decisao.dart
+++ dart/logica-de-programacao-com-dart/aula06 - estrutura de decisao/estrutura-decisao.dart
@@ -1,12 +1,22 @@
 main(){
   // também chamada de estrutura condicional
 
-  var conta = (1 == 0); // true
+  var conta = (1 == 0); // false
 
   if(conta){ // mesmo de if(conta == true){
     print("Isso é verdadeiro.");
   }
   else{
-    print('${conta} é falso.');
+    print("Isso é falso");
   }
 }
+
+/*
+  = -> recebe valor
+  == -> igual
+  != -> diferente
+  > -> maior que
+  < -> menor que
+  >= -> maior ou igual a
+  <= -> menor ou igual a
+*/
